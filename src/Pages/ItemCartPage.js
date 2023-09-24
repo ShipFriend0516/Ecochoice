316ef7badefa08a7c689da1d40ada328c9f5f024
JeongwooSeo <98446924+ShipFriend0516@users.noreply.github.com>
2023-09-24 21:04:09 +0900

    장바구니 페이지 틀

--- src/Pages/ItemCartPage.js
+++ src/Pages/ItemCartPage.js
@@ -1,5 +1,22 @@
-const ItemCartPage = () => {
-  return <div>장바구니 페이지</div>;
+import { useState } from "react";
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+import styles from "../Styles/ItemCartPage.module.css";
+
+const ItemCartPage = ({ user }) => {
+  // props로 유저를 받아와, 유저마다 다른 장바구니를 적용
+  const [cart, setCart] = useState([]);
+
+  return (
+    <div className={`${styles.ItemCartPageWrapper}`}>
+      <Header isFixed={true} />
+      <div className={`${styles.ItemCartWrapper}`}>
+        <div className={`${styles.ItemCartTitle}`}>장바구니 페이지</div>
+        <div className={`${styles.ItemCart}`}></div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default ItemCartPage;
