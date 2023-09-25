616a14005af7b5d6bf5df428e66e544c3c29454a
Oracionspace <shipfriend4371@naver.com>
2023-09-25 23:32:55 +0900

    로그인/회원가입 유효성 검사 추가

--- src/Components/LoginModal.js
+++ src/Components/LoginModal.js
@@ -6,22 +6,53 @@ const LoginPage = ({ loginOnClick, isOpen }) => {
   const [isRegistered, setIsRegistered] = useState(false);
   const [id, setID] = useState("");
   const [pw, setPW] = useState("");
-
+  const [pwCheck, setPWCheck] = useState("");
+  const [error, setError] = useState("");
   const canvasOnClick = () => {
     loginOnClick();
   };
   const onSubmit = (e) => {
     e.preventDefault();
-    try {
-      const response = axios.get(`http://localhost:3001/`);
-    } catch (e) {
-      console.error("로그인 실패:", e);
+    if (isRegistered) {
+      try {
+        const response = axios.get(`http://localhost:3001/`);
+      } catch (e) {
+        console.error("로그인 실패:", e);
+      }
+    } else {
+      try {
+        let validateCode = RegisterValidate();
+        if (validateCode === true) {
+          console.log("유효성 검사 통과");
+        } else {
+          console.log("유효성 검사 실패");
+          setError(validateCode);
+          console.log(validateCode);
+        }
+      } catch (err) {
+        console.error("회원가입 실패:", e);
+      }
     }
+  };
+
+  const RegisterValidate = () => {
+    // 회원가입 유효성 검사 함수
+    // 비밀번호 규칙 4글자 이상, 아이디 4글자 이상
+    if (id.length < 8 && pw !== pwCheck) {
+      return 100;
+    } else if (id.length < 8) {
+      return 101;
+    } else if (pw !== pwCheck) {
+      return 102;
+    }
+
+    return true;
   };
 
   const onRegisterClick = (e) => {
     e.preventDefault();
     setIsRegistered((prev) => !prev);
+    setError("");
   };
 
   const onChangeID = (e) => {
@@ -40,15 +71,31 @@ const LoginPage = ({ loginOnClick, isOpen }) => {
             <div className="">
               <label htmlFor="id">아이디</label>
               <input onChange={onChangeID} id="id" className={"form-control"} type="email"></input>
+              {error === 100 || error === 101 ? (
+                <small className={styles.errorMsg}>아이디는 8글자 이상이어야합니다.</small>
+              ) : null}
             </div>
             <div>
               <label htmlFor="pw">비밀번호</label>
-              <input id="pw" className={"form-control"} type="password"></input>
+              <input
+                onChange={(e) => setPW(e.target.value)}
+                id="pw"
+                className={"form-control"}
+                type="password"
+              ></input>
             </div>
             {!isRegistered && (
               <div>
                 <label htmlFor="pw">비밀번호 확인</label>
-                <input id="pw" className={"form-control"} type="password"></input>
+                <input
+                  onChange={(e) => setPWCheck(e.target.value)}
+                  id="pwCheck"
+                  className={"form-control"}
+                  type="password"
+                ></input>
+                {error === 100 || error === 102 ? (
+                  <small className={styles.errorMsg}>비밀번호가 일치하지 않습니다.</small>
+                ) : null}
               </div>
             )}
             <div className="d-flex justify-content-between gap-3">
